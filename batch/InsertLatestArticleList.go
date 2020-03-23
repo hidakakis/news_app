18172b607ef969ec8303fd7b0c1f7cbc65531f09
ponyo877 <keisuke877jp@yahoo.co.jp>
2020-03-24 00:05:09 +0900

    change insert to postgres part in InsertLatestArticleList.go

--- batch/InsertLatestArticleList.go
+++ batch/InsertLatestArticleList.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
@@ -16,24 +17,23 @@ type SiteInfo struct {
 	latestDate string
 }
 
-// SiteRecord is site infomation on DB
+// SiteRecord is article infomation for DB
 type SiteRecord struct {
-	title       string
-	URL         string
-	image       string
-	publishedAt string
-	updateDate  string
+	title      string
+	URL        string
+	image      string
+	updateDate string
+	siteID     int
+}
+
+// EsRecord is article infomation for ElasticSearch
+type EsRecord struct {
+	ID    int
+	title string
 }
 
 // Db is PostgreSQL Instance
 var Db *sql.DB
-
-func imageFromFeed(feed string) string {
-	reader := strings.NewReader(feed)
-	doc, _ := goquery.NewDocumentFromReader(reader)
-	ImageURL, _ := doc.Find("img").Attr("src")
-	return ImageURL
-}
 
 func initDB() {
 	var err error
@@ -43,12 +43,51 @@ func initDB() {
 	}
 }
 
-func insertLatestArticleToDB(Article string) {
+func getImageFromFeed(feed string) string {
+	reader := strings.NewReader(feed)
+	doc, _ := goquery.NewDocumentFromReader(reader)
+	ImageURL, _ := doc.Find("img").Attr("src")
+	return ImageURL
+}
 
+func registerLatestArticleToDB(articleList []SiteRecord) []EsRecord {
+	sql01_02 := "INSERT INTO /* sql01_02 */ article_tbl (title, url, update_date, click, site_id) VALUES ($1, $2, $3, $4, $5) RETURNING (id, title)"
+	stmt, err := Db.Prepare(sql01_02)
+	if err != nil {
+		panic(err)
+	}
+	defer stmt.Close()
+	esRecordList := []EsRecord{}
+	var esRecord EsRecord
+	for _, article := range articleList {
+		err = stmt.QueryRow(article.title, article.URL, article.updateDate, 0, article.siteID).Scan(&esRecord.ID, &esRecord.title)
+		esRecordList = append(esRecordList, esRecord)
+	}
+	return esRecordList
+	/*
+		--past python code--
+		SQL_02_03 = 'SELECT max(id) FROM article_tbl'
+		cur.execute(SQL_02_03)
+		max_id = cur.fetchone()
+		SQL_02_04 = 'INSERT INTO article_tbl (title, url, update_date, click, site_id) VALUES %s'
+		extras.execute_values(cur, SQL_02_04, update_article_list)
+		SQL_02_05 = 'SELECT id, title FROM article_tbl where id > %s'
+		cur.execute(SQL_02_05, (max_id, ))
+		update_es_list = cur.fetchall()
+		save_elasticsearch(update_es_list)
+		--past python code--
+	*/
+}
+
+func registerLatestArticleToES(articleList []EsRecord) {
+	for _, article := range articleList {
+		fmt.Println("registerLatestArticleToES:", article.ID, article.title)
+		// TBD
+	}
 }
 
 func updateLatestDate(siteID int, updateDate string) {
-	// pass
+	// TBD
 }
 
 func getSiteInfoList() []SiteInfo {
@@ -56,13 +95,13 @@ func getSiteInfoList() []SiteInfo {
 	siteinfolist := []SiteInfo{}
 	sql01_01 := "SELECT /* sql01_01 */ ID, title, rssURL, latestDate FROM site_tbl"
 
-	getSiteInfoListFromDB, err := Db.Query(sql01_01)
+	selectSiteInfoList, err := Db.Query(sql01_01)
 	if err != nil {
 		panic(err)
 	}
-	defer getSiteInfoListFromDB.Close()
-	for getSiteInfoListFromDB.Next() {
-		if err := getSiteInfoListFromDB.Scan(
+	defer selectSiteInfoList.Close()
+	for selectSiteInfoList.Next() {
+		if err := selectSiteInfoList.Scan(
 			&siteinfo.ID,
 			&siteinfo.title,
 			&siteinfo.rssURL,
@@ -81,10 +120,6 @@ func getSiteInfoList() []SiteInfo {
 	return siteinfolist
 }
 
-func getUpdateDate() {
-
-}
-
 func main() {
 	siteinfolist := getSiteInfoList()
 	feedparser := gofeed.NewParser()
@@ -96,11 +131,11 @@ func main() {
 		items := feed.Items
 		for _, item := range items {
 			feedmap := SiteRecord{
-				title:       item.Title,
-				URL:         item.Link,
-				image:       imageFromFeed(item.Content),
-				publishedAt: "tbd",
-				updateDate:  item.Updated,
+				title:      item.Title,
+				URL:        item.Link,
+				image:      getImageFromFeed(item.Content),
+				updateDate: item.Updated,
+				siteID:     siteinfo.ID,
 			}
 			if feedmap.updateDate > siteinfo.latestDate {
 				feedArray = append(feedArray, feedmap)
@@ -111,4 +146,6 @@ func main() {
 			}
 		}
 	}
+	esRecord := registerLatestArticleToDB(feedArray)
+	registerLatestArticleToES(esRecord)
 }
